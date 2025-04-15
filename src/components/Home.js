2ab3cb52001c1ba828c20844d048e07c5549132a
somto <somto565@gmail.com>
2025-04-15 14:50:52 +0100

    fix: update social media links to open in new tab and adjust CSS styles

--- src/components/Home.js
+++ src/components/Home.js
@@ -41,17 +41,20 @@ const Home = ({ dark }) => {
           <span>Follow me:</span>
           <ul>
             <li>
-              <a href="https://www.behance.net/beccasimon1">
+              <a href="https://www.behance.net/beccasimon1"
+              target="_blank">
                 <img className="svg" src="img/svg/social/be.svg" alt="" />
               </a>
             </li>
             <li>
-              <a href="https://dribbble.com/Nwovu-Rebecca">
+              <a href="https://dribbble.com/Nwovu-Rebecca"
+              target="_blank">
                 <img className="svg" src="img/svg/social/dribbble.svg" alt="" />
               </a>
             </li>
             <li>
-              <a href="https://www.instagram.com/becca.sznn?igsh=a2Y4dGlwN3hxd3Nv">
+              <a href="https://www.instagram.com/becca.sznn?igsh=a2Y4dGlwN3hxd3Nv"
+              target="_blank">
                 <img
                   className="svg"
                   src="img/svg/social/instagarm.svg"
